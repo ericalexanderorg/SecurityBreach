b0cb36fcb97750d4dab50f8032f83d464b53f7a9
Eric Alexander <eric.alexander@thetradedesk.com>
2019-10-06 10:11:46 -0600

    add-intake-form-1

--- UI/v1/src/App.js
+++ UI/v1/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import './App.css';
+
 import dataProvider from './dataProvider';
-import { BreachList } from './breaches';
+import { BreachList, BreachCreate } from './breaches';
 import { Dashboard } from './dashboard';
 
 const App = () => (  
   <Admin dashboard={Dashboard} title="SecurityBreach" dataProvider={dataProvider}>
-      <Resource name="breaches" list={BreachList} />
+      <Resource name="breaches" list={BreachList} create={BreachCreate} />
   </Admin>
 );
 
--- UI/v1/src/breaches.js
+++ UI/v1/src/breaches.js
@@ -7,26 +7,32 @@ import {
     List, 
     Datagrid, 
     TextField,
-    BooleanField,
-    ChipField,
     ArrayField,
-    SingleFieldList,
     UrlField,
-    ReferenceArrayInput,
-    BooleanInput,
-    AutocompleteArrayInput,
-    ReferenceArrayField, 
-    DateField
+    SimpleForm,
+    Create,
+    ReferenceInput, 
+    SelectInput,
+    LongTextInput,
+    DateInput,
+    TabbedForm,
+    FormTab,
+    NumberInput,
+    ShowButton
 } from 'react-admin';
+import { Button, Card, CardActions, CardContent, withStyles, Typography } from '@material-ui/core';
+import QueueIcon from '@material-ui/icons/Queue';
+import HelpIcon from '@material-ui/icons/Help';
+//import FlatButton from 'material-ui/FlatButton';
 
-const PostFilter = (props) => (
+const BreachFilter = (props) => (
     <Filter {...props}>
         <TextInput label="Search" source="q" alwaysOn />
     </Filter>
 );
 
 export const BreachList = props => (
-    <List {...props} filters={<PostFilter />} bulkActions={false} sort={{ field: 'Start Date', order: 'DESC' }}>
+    <List {...props} filters={<BreachFilter />} bulkActions={false} sort={{ field: 'Start Date', order: 'DESC' }}>
         <Datagrid hasBulkActions={false} expand={<BreachShow />}>
             <TextField source="Start Date" />
             <TextField source="entity" />
@@ -50,4 +56,76 @@ export const BreachShow = (props) => (
             </ArrayField>
         </SimpleShowLayout>
     </Show>
+);
+
+var Aside = () => (
+    <div style={{ width: "50%", 'margin-left': '1em' }}>
+        <Card>
+            <CardContent>
+                <Typography variant="headline" component="h2">
+                    Adding new breach data, the simple way
+                </Typography>
+                <Typography component="p" style={{ 'margin-top': '1em' }}>
+                    Submit a Github Issue in our project with a link to more information and a project contributor will review, classify, and add to our data set.
+                </Typography>
+            </CardContent>
+        </Card>
+        <Card style={{ 'margin-top': '1em' }}>
+            <CardContent>
+                <Typography variant="headline" component="h2">
+                    Adding new breach data, with more information.
+                </Typography>
+                <Typography component="p" style={{ 'margin-top': '1em' }}>
+                    Use this form to generate breach data in json format and either submit an issue or a pr to add it to the data set.
+                </Typography>
+            </CardContent>
+        </Card>
+        <Card style={{ 'margin-top': '1em' }}>
+            <CardContent>
+                <Typography variant="headline" component="h2">
+                    Form JSON Output
+                </Typography>
+                <Typography key={this.state.formJSON} component="pre" style={{ 'margin-top': '1em' }}>
+                    {JSON.stringify(this.state.formJSON, null, 4)}
+                </Typography>
+            </CardContent>
+        </Card>
+    </div>
+)
+
+var formJson = {};
+
+const validateBreachCreation = (values) => {
+    console.log(values)
+    formJson = values
+    //this.setState({ state: this.state });
+    const errors = {};
+    if (!values.firstName) {
+        errors.firstName = ['The firstName is required'];
+    }
+    if (!values.age) {
+        errors.age = ['The age is required'];
+    } else if (values.age < 18) {
+        errors.age = ['Must be over 18'];
+    }
+    errors.entity = ['Must be over 18'];
+    return errors
+};
+
+export const BreachCreate = (props) => (
+    <Create title={'Add new breach data'} aside={<Aside />} {...props}>
+            <SimpleForm saving={true} validate={validateBreachCreation}>
+                <NumberInput source="Year" label="Start Year" defaultValue={2019} />
+                <NumberInput source="Month" label="Start Month"  defaultValue={10} />
+                <TextInput source="entity" defaultValue={"Who was breached?"} />
+                <LongTextInput source="summary" defaultValue={"Short summary of what happened"} />
+                <TextInput source="Tags.actor" defaultValue={"?"} />
+                <TextInput source="Tags.initial-access" defaultValue={"?"} />
+                <TextInput source="Tags.motive" defaultValue={"?"} />
+                <NumberInput source="Tags.impacted-user-count" defaultValue={0}/>
+                <Typography component="pre" style={{ 'margin-top': '1em' }}>
+                    {JSON.stringify(formJson, null, 4)}
+                </Typography>
+            </SimpleForm>
+    </Create>
 );
