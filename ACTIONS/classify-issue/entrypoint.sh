78ef8e311c8e9275914d38bf77a91687a6238ef4
Eric Alexander <eric.alexander@thetradedesk.com>
2019-11-16 10:25:29 -0700

    classify-action-4

--- ACTIONS/classify-issue/entrypoint.sh
+++ ACTIONS/classify-issue/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # CD to where actions/checkout adds code
-cd $GITHUB_WORKSPACE
+cd "$GITHUB_WORKSPACE" || exit
 
 # Set issue number and title
 GITHUB_ISSUE_NUMBER=$(jq --raw-output .issue.number "$GITHUB_EVENT_PATH")
@@ -13,28 +13,22 @@ echo "$GITHUB_ISSUE_BODY"
 URL=$(echo "$GITHUB_ISSUE_BODY" | grep -o "https\?://[a-zA-Z0-9./?=_-]*")
 echo "$URL"
 
-# Create a branch with the
-BRANCH_NAME="auto-classified-issue-$GITHUB_ISSUE_NUMBER"
-
-git checkout -b $BRANCH_NAME
-
-# Setup a virtual environment and install dependencies.
-python3 -m venv ./.ve
-source ./.ve/bin/activate
-pip3 install -r ML/SCRIPTS/requirements-classify-sklearn.txt
+# Install dependencies.
+pip3 install -r ./ML/SCRIPTS/requirements-classify-sklearn.txt
 
 # Prep dataset file by removing last 3 lines that contain: }]}
-$DATA_FILE="UI/v1/src/dataProvider/security-breach-v1.json"
-$TEMP=$(cat $DATA_FILE | head -n -3 file)
+DATA_FILE="UI/v1/src/dataProvider/security-breach-v1.json"
+TEMP=$(cat $DATA_FILE | head -n -3 file)
 echo "$TEMP" > $DATA_FILE
 echo "   }," >> $DATA_FILE
 
 # Classify and append to dataset
-python ML/SCRIPTS/classify-sklearn.py $URL $GITHUB_ISSUE_TITLE >> $DATA_FILE
+JSON=$(python ML/SCRIPTS/classify-sklearn.py "$URL" "$GITHUB_ISSUE_TITLE")
+echo "$JSON" >> $DATA_FILE
 
 # Add last two lines we removed earlier
-echo "  ]" >> $DATA_FILE
-echo "}" >> $DATA_FILE
+echo "  ]" >> "$DATA_FILE"
+echo "}" >> "$DATA_FILE"
 
 # Pass issue number to next step using output variable
-echo ::set-output name=issue_number::$GITHUB_ISSUE_NUMBER
+echo ::set-output name=issue_number::"$GITHUB_ISSUE_NUMBER"
